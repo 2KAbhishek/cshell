7c61d551782a4d5eaeabdebd84578eaac657558a
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-30 16:06:51 +0530

    Add file redirection handler

--- src/cshell.c
+++ src/cshell.c
@@ -99,3 +99,42 @@ void signalHandler_int(int p)
     }
 }
 
+void fileIOHandler(char *args[], char *inputFile, char *outputFile, int option)
+{
+    int err = -1;
+
+    int fileDescriptor;
+
+    if ((pid = fork()) == -1)
+    {
+        printf("Child process could not be created\n");
+        return;
+    }
+    if (pid == 0)
+    {
+        if (option == 0)
+        {
+            fileDescriptor = open(outputFile, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+            dup2(fileDescriptor, STDOUT_FILENO);
+            close(fileDescriptor);
+        }
+        else if (option == 1)
+        {
+            fileDescriptor = open(inputFile, O_RDONLY, 0600);
+            dup2(fileDescriptor, STDIN_FILENO);
+            close(fileDescriptor);
+            fileDescriptor = open(outputFile, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+            dup2(fileDescriptor, STDOUT_FILENO);
+            close(fileDescriptor);
+        }
+
+        setenv("parent", getcwd(currentDirectory, 1024), 1);
+
+        if (execvp(args[0], args) == err)
+        {
+            printf("err");
+            kill(getpid(), SIGTERM);
+        }
+    }
+    waitpid(pid, NULL, 0);
+}
