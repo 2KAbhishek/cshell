fd5e6bc4b8d4c3e1de4d34e1cffeaeb6620ec3e1
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-30 16:08:52 +0530

    Add cd implementation

--- src/cshell.c
+++ src/cshell.c
@@ -353,3 +353,22 @@ void showHistory()
     }
 }
 
+int changeDir(char *args[])
+{
+    if (args[1] == NULL)
+    {
+        chdir(getenv("HOME"));
+        return 1;
+    }
+
+    else
+    {
+        if (chdir(args[1]) == -1)
+        {
+            printf(" %s: no such directory\n", args[1]);
+            return -1;
+        }
+    }
+    return 0;
+}
+
