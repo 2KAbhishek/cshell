94cf79cc4f86f78cfb1e4d502035669e79919d25
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-30 16:05:59 +0530

    Add shellInit, sets up interactive tty

--- src/cshell.c
+++ src/cshell.c
@@ -39,3 +39,40 @@ int numCommands = 0;
 void signalHandler_child(int p);
 void signalHandler_int(int p);
 
+void shellInit()
+{
+    GBSH_PID = getpid();
+
+    GBSH_IS_INTERACTIVE = isatty(STDIN_FILENO);
+
+    if (GBSH_IS_INTERACTIVE)
+    {
+        while (tcgetpgrp(STDIN_FILENO) != (GBSH_PGID = getpgrp()))
+            kill(GBSH_PID, SIGTTIN);
+
+        act_child.sa_handler = signalHandler_child;
+        act_int.sa_handler = signalHandler_int;
+
+        sigaction(SIGCHLD, &act_child, 0);
+        sigaction(SIGINT, &act_int, 0);
+
+        setpgid(GBSH_PID, GBSH_PID);
+        GBSH_PGID = getpgrp();
+        if (GBSH_PID != GBSH_PGID)
+        {
+            printf("Error, the shell is not process group leader");
+            exit(EXIT_FAILURE);
+        }
+
+        tcsetpgrp(STDIN_FILENO, GBSH_PGID);
+
+        tcgetattr(STDIN_FILENO, &GBSH_TMODES);
+
+        currentDirectory = (char *)calloc(1024, sizeof(char));
+    }
+    else
+    {
+        printf("Could not make the shell interactive.\n");
+        exit(EXIT_FAILURE);
+    }
+}
