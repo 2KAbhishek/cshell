92d7615e51fa269b7ac68bc59eed22f6ddd216c8
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-08-06 16:07:48 +0530

    Cleanup line before tokenizing

--- src/cshell.c
+++ src/cshell.c
@@ -492,6 +492,50 @@ int commandHandler(char *args[])
     return 1;
 }
 
+char *str_replace(char *orig, char *rep, char *with)
+{
+    char *result;  // the return string
+    char *ins;     // the next insert point
+    char *tmp;     // varies
+    int len_rep;   // length of rep (the string to remove)
+    int len_with;  // length of with (the string to replace rep with)
+    int len_front; // distance between rep and end of last rep
+    int count;     // number of replacements
+
+    // sanity checks and initialization
+    if (!orig || !rep)
+        return NULL;
+    len_rep = strlen(rep);
+    if (len_rep == 0)
+        return NULL; // empty rep causes infinite loop during count
+    if (!with)
+        with = "";
+    len_with = strlen(with);
+
+    // count the number of replacements needed
+    ins = orig;
+    for (count = 0; tmp = strstr(ins, rep); ++count)
+    {
+        ins = tmp + len_rep;
+    }
+
+    tmp = result = malloc(strlen(orig) + (len_with - len_rep) * count + 1);
+
+    if (!result)
+        return NULL;
+
+    while (count--)
+    {
+        ins = strstr(orig, rep);
+        len_front = ins - orig;
+        tmp = strncpy(tmp, orig, len_front) + len_front;
+        tmp = strcpy(tmp, with) + len_with;
+        orig += len_front + len_rep; // move to next "end of rep"
+    }
+    strcpy(tmp, orig);
+    return result;
+}
+
 int main(int argc, char *argv[], char **envp)
 {
     char line[MAXLINE];
@@ -520,6 +564,7 @@ int main(int argc, char *argv[], char **envp)
         memset(line, '\0', MAXLINE);
 
         fgets(line, MAXLINE, stdin);
+        char *command = str_replace(line, ";", " ;");
 
         // Exit if the input is EOF
         if (feof(stdin))
@@ -527,7 +572,7 @@ int main(int argc, char *argv[], char **envp)
             exit(0);
         }
 
-        if ((tokens[0] = strtok(line, " \n\t")) == NULL)
+        if ((tokens[0] = strtok(command, " \n\t")) == NULL)
         {
             continue;
         }
